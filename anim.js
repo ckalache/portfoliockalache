56051605b6df3682daf7380c566aa9e3ed4d0af4
Felipe Vieira <mrchampz@outlook.com>
2024-11-14 13:55:57 -0300

    initial hero animations

--- anim.js
+++ anim.js
@@ -1,5 +1,50 @@
 import { animate, scroll } from "https://cdn.jsdelivr.net/npm/framer-motion@11.11.11/dom/+esm";
 
+/* Hero animations */
+
+const heroRect = document.querySelector(".main .hero .spotlight .rect");
+const heroCreative = document.querySelector(".main .hero .spotlight .text #creative");
+const heroWeb = document.querySelector(".main .hero .spotlight .text #web");
+const heroGraphic = document.querySelector(".main .hero .spotlight .text #graphic");
+const heroDesigner = document.querySelector(".main .hero .spotlight .text #designer");
+const heroIndicator = document.querySelector(".main .hero .spotlight .indicator");
+
+animate(
+  heroRect,
+  { x: [-window.innerWidth, 0] },
+  { ease: "easeOut", duration: 1.2 }
+);
+
+animate(
+  heroCreative,
+  { y: [-90, 0], opacity: [0, 1] },
+  { ease: "easeOut", duration: 0.7 }
+);
+
+animate(
+  heroWeb,
+  { x: [-160, 0], opacity: [0, 1] },
+  { ease: "easeOut", duration: 0.7 }
+);
+
+animate(
+  heroGraphic,
+  { x: [180, 0], opacity: [0, 1] },
+  { ease: "easeOut", duration: 0.7 }
+);
+
+animate(
+  heroDesigner,
+  { y: [120, 0], opacity: [0, 1] },
+  { ease: "easeOut", duration: 0.7 }
+);
+
+animate(
+  heroIndicator,
+  { x: [window.innerHeight / 2, 0], y: [-500, 0] },
+  { ease: "easeOut", duration: 1.3 }
+);
+
 /* "Selected Projects" section title animation  */
 
 const projectsTitle = document.querySelector(".main .content > h2");
